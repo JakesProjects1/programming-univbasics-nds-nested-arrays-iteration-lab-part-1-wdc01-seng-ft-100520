45e375944cc7e9aea1a0b7936036c7e9ca06536f
Jacob Kagon <jacob@kagon.net>
2020-09-16 16:36:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,16 @@ def find_even_values(src)
   
   
   row_index = 0
-  while row_index
-  
+  while row_index < array.count do
+    element_index = 0
+    while element_index < array[row_index].count
+      if element_index % 2 == 0 
+        p array[row_index][element_index]
+      end
+      element_index += 1
+    end
+    row_index += 1 
+  end
   
   
   
